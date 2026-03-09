52ef949f1a9293124ab7c52b7aabe81833b21c14
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-03-09 13:56:47 +0500

    settings.py fixed

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
